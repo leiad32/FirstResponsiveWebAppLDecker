242ec2567537ab56158cfde46563840e7d1261e4
Leia Decker <lmdecker@dmacc.edu>
2021-02-01 21:23:56 -0600

    Add project files.

---
+++ FirstResponsiveWebAppDecker/Controllers/HomeController.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using FirstResponsiveWebAppDecker.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FirstResponsiveWebAppDecker.Controllers
+{
+    public class HomeController : Controller
+    {
+        [HttpGet]
+        public IActionResult Index()
+        {
+            ViewBag.Age = 0;
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Index(AgeModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                ViewBag.Age = model.AgeThisYear();
+            }
+            else
+            {
+                ViewBag.Age = 0;
+            }
+            return View(model);
+        }
+    }
+}
---
+++ FirstResponsiveWebAppDecker/Models/AgeModel.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+
+namespace FirstResponsiveWebAppDecker.Models
+{
+    public class AgeModel
+    {
+        const int BEGIN_RANGE = 1921;
+        const int END_RANGE = 2021;
+        int CURRENT = DateTime.Now.Year;
+        [Required(ErrorMessage ="Please enter a name.")]
+        [StringLength (20, ErrorMessage = "Name cannot be more than 20 letters.")]
+        public string Name { get; set; }
+        [Required(ErrorMessage = "Please enter a year between 1921-2021.")]
+        [Range (BEGIN_RANGE,END_RANGE, ErrorMessage = "Birth year must be between 1921 and 2021.")]
+        public int BirthYear { get; set; }
+
+        public int AgeThisYear()
+        {
+            int age = CURRENT - BirthYear;
+            return age;
+        }
+    }
+}
