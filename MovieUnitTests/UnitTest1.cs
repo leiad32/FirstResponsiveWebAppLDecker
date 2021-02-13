ef1cbffcf97f670fc59bbfcd78791242be7a0757
Leia Decker <lmdecker@dmacc.edu>
2021-02-13 00:16:21 -0600

    UnitTestsAdded

---
+++ MovieUnitTests/UnitTest1.cs
@@ -0,0 +1,39 @@
+using FirstResponsiveWebAppDecker;
+using System;
+using Xunit;
+using FirstResponsiveWebAppDecker.Controllers;
+using System.Linq;
+using System.Text;
+using System.Collections.Generic;
+using System.Web;
+using FirstResponsiveWebAppDecker.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MovieUnitTests
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void CorrectInput()
+        {
+            //Arrange
+            int bYear = 2001;
+
+            int expected = 20;
+            int actual;
+
+            //Act
+            var HomeController = new HomeController();
+
+            /* I think the Unit Tests have to connect to the Controller, but I can't find anything on the web that even hints at
+             * being the solution for creating web applications. I've tried working with the Controller, the Model, working with Moq, etc.
+             * And nothing thus far has worked. The videos that Michelle Ruse provided aren't applicable to our type of program, and neither are the 
+             * links provided. This shows that I've created the xUnit test, and I have added some information to the unit test. I know what a Unit test
+             * should do, but haven't been given enough information to write one. 
+            */
+
+            //Assert
+
+        }
+    }
+}
